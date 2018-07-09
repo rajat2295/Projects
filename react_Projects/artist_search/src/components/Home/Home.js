e96a1a1a593de5478480613b1449f17bfbfd76d9
rajat <rajatratra@inshorts.com>
2018-07-09 17:15:22 +0530

    artist search

---
+++ react_Projects/artist_search/src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import Modal from '../Modal/Modal'
+
+
+class Home extends Component{
+   
+
+    render(){
+        return(
+            <div>hello</div>
+        )
+    }
+} 
+
+export default Home;
---
+++ react_Projects/artist_search/src/components/Modal/Modal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Modal = (props) => {
+    return(
+        <div >
+            <Modal />
+            {props.children}
+        </div>
+    )
+
+}
+
+export default Modal;
