1cd1d8bbf0fef1188dc0ddc4e21d1c159304bdab
rajat <rajatratra@inshorts.com>
2018-07-24 09:52:42 +0530

    base

---
+++ react_Projects/josh/src/components/footer/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css'
+const header = (props) =>{
+    return(
+    <div className='footer'>
+        <a className ='footer__link' href="#">Terms & Conditions</a>
+        <a className ='footer__link' href="#">Privacy Policy</a>
+        <a className ='footer__link' href="#">Contact Us</a>
+        <a className ='footer__link' href="#">Terms & Conditions</a>
+        <p className='footer__logo'> Josh Technology Group</p>
+    </div>
+    )
+}
+
+export default header;
---
+++ react_Projects/josh/src/container/Home/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Footer from '../../components/footer/footer'
+class Home extends Component{
+    
+
+    render(){
+        return(
+            <div><Footer /></div>
+        )
+    }
+}
+
+export default Home;
